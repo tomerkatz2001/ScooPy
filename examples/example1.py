f4f91b7c436da08a5ac41dffe5bd7c3cb4621905
tomer katz <64071448+tomerkatz2001@users.noreply.github.com>
2022-12-05 23:58:35 +0200

    Create example1.py

---
+++ examples/example1.py
@@ -0,0 +1,18 @@
+# In this example we can see that y by itself is not good condidate for e_cond since the user added the second example in synth number 2
+# in contrast to e_cond the synthesizer choose in the first synth.
+
+y = 2
+z = 3
+
+#! Start of synth number 1 of: x
+#! 1) y = 2, z = 3 => x = 0
+#! 2) y = 3, z = 3 => x = 5 #manually added
+if y > 2:
+    x = z + 2
+else: 
+    #! Start of synth number 2 of: x
+    #! 1) y = 2, z = 3 => x = 0
+    #! 2) y = 4, z = 3 => x = 5 #manually added
+    x = y + z
+    #! End of synth number 2
+#! End of synth number 1
