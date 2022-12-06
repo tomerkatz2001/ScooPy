9614ed255be2002e401de45bbc5e54ae2ba0a8e3
tomer katz <64071448+tomerkatz2001@users.noreply.github.com>
2022-12-06 12:44:55 +0200

    Create example4.py

---
+++ examples/example4.py
@@ -0,0 +1,14 @@
+# With the given partition, The only job we have to to is to find e_cond that satisfy the partition.
+
+def drop(l, x):
+    res = []
+    for i in l:
+        if ??:
+            #! 1) i = 4 => res = [4]
+            #! 2) i = 7 ,res_in = [4] => res = [4, 7]
+            res.append(i)
+        else:
+            #! 1) i = 5 ,res_in = [1] => res = [1] 
+            continue
+l = [4, 5, 7]     
+drop(l, 5)
